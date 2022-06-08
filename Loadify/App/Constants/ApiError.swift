e5e3d0ec1e604177a3d2818b15ba7c3e2f45224c
VishwaiOSDev <vishwa300499@icloud.com>
2022-06-08 16:11:08 +0530

    ApiError file git error handled

---
+++ Loadify/App/Constants/ApiError.swift
@@ -0,0 +1,12 @@
+//
+//  ApiError.swift
+//  Loadify
+//
+//  Created by Vishweshwaran on 08/06/22.
+//
+
+import Foundation
+
+struct ApiError {
+    static let notValidYouTubeDomain = "Error: Not a YouTube domain"
+}
