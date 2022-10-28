b73dce6449981ae1e423eab74a7dddf40890479e
VishwaiOSDev <vishwa300499@icloud.com>
2022-10-28 20:45:53 +0530

    Added MockURLViewModel and unit tested

--- LoadifyTests/Mock/MockApiService.swift
+++ LoadifyTests/Mock/MockApiService.swift
@@ -8,7 +8,28 @@
 import Foundation
 @testable import Loadify
 
-final class MockApiService: DataService, Mockable {
+class URLMockViewModel: Detailable {
+    
+    var details: VideoDetails? = nil
+    var showLoader: Bool = false
+    var error: Error? = nil
+    var shouldNavigateToDownload: Bool = false
+    
+    private var apiService: FetchService
+    
+    init(apiService: FetchService) {
+        self.apiService = apiService
+    }
+    
+    func getVideoDetails(for url: String) async {
+        do {
+            let response = try await apiService.fetchVideoDetailsFromApi(for: url)
+            details = response
+        } catch { }
+    }
+}
+
+final class MockApiService: FetchService, Mockable {
     
     func fetchVideoDetailsFromApi(for url: String) async throws -> VideoDetails {
         if url == "https://www.youtube.com/watch?v=66XwG1CLHuU" {
