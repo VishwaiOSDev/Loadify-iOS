fd0b0b057ab6cf6475e037b32c54080279944f11
VishwaiOSDev <vishwa300499@icloud.com>
2022-11-06 20:43:52 +0530

    Removed unwanted errors

--- Loadify/App/Enums/Errors.swift
+++ Loadify/App/Enums/Errors.swift
@@ -7,50 +7,12 @@
 
 import Foundation
 
-enum ServerError: Error, LocalizedError {
-    case notValidDomain
-    case internalServerError
-    case requestedQualityUnavailable
-    case durationTooHigh
-    case decodeFailed
-    
-    var errorDescription: String? {
-        switch self {
-        case .notValidDomain: return "Error: Not a YouTube domain"
-        case .internalServerError: return "Something went wrong"
-        case .requestedQualityUnavailable: return "The requested quality is not supported"
-        case .durationTooHigh: return "Requested video length is too high"
-        case .decodeFailed: return "Failed to decode data"
-        }
-    }
-}
-
-enum DetailsError: Error, LocalizedError {
-    case emptyUrl
-    case invaildApiUrl
-    case notVaildYouTubeUrl
-    
-    var errorDescription: String? {
-        switch self {
-        case .emptyUrl: return "URL cannot be empty"
-        case .invaildApiUrl: return "This is not valid Url"
-        case .notVaildYouTubeUrl: return "This is not a valid YouTube Url"
-        }
-    }
-}
-
 enum DownloadError: Error, LocalizedError {
     case notCompatible
-    case fatalError
-    case qualityNotAvailable
-    case durationTooHigh
     
     var errorDescription: String? {
         switch self {
         case .notCompatible: return "This video is not compatible to save"
-        case .fatalError: return "Something went worng. Please try again later"
-        case .qualityNotAvailable: return "The request quality is not available"
-        case .durationTooHigh: return "Duration is greater than 15 minutes"
         }
     }
 }
