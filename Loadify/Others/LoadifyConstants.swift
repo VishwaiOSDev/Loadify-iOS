e212d8c13a9cc4fa800f8c6476ed38cacf656971
VishwaiOSDev <vishwar3001@gmail.com>
2025-09-26 16:08:22 -0400

    feat: updated the ui to support facebook url

--- Loadify/Others/LoadifyConstants.swift
+++ Loadify/Others/LoadifyConstants.swift
@@ -33,7 +33,7 @@ struct Loadify {
         static let loading = "LOADING"
         static let downloadedTitle = "Downloaded Successfully"
         
-        static let loadifySubTitle = "Download Instagram Reels, Posts, Stories and TikTok Video with Ease!"
+        static let loadifySubTitle = "Download Instagram Reels, Posts, Stories, Facebook Videos and TikTok Videos with Ease!"
         
         static let photosAccessTitle = "Photos access is required"
         static let photosAccessSubtitle = "To enable access, go to Settings > Privacy > Photos and turn on All Photos access for this app."
